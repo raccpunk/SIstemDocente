7f4b361adca7fd002fd249174dba3f4ac582ef1a
raccpunk <66329111+raccpunk@users.noreply.github.com>
2021-06-01 12:38:38 -0500

    add attendance

---
+++ resources/views/ListaAsistencia.blade.php
@@ -0,0 +1,30 @@
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"  rel="stylesheet">
+<div class="card-header"></div>
+<ul class="navbar-nav ml-auto">
+<div class="card-body">
+
+</div>
+<div class="container">
+    <div class="row">
+
+      <div class="col text-center">
+        <select name="OS">
+            <option value="1">Materia</option>
+            <option value="2">...</option>
+         </select>
+         <select name="OS">
+            <option value="1">Grado</option>
+            <option value="2">Primer Grado</option>
+            <option value="2">Segundo Grado</option>
+            <option value="2">Tercer Grado</option>
+         </select>
+         <select name="OS">
+            <option value="1">Grupo</option>
+            <option value="2">A</option>
+            <option value="3">B</option>
+         </select>
+         <br>
+         <br>
+         <a href="" class="btn btn-success ">Descargar Lista de asistencia</a>
+
